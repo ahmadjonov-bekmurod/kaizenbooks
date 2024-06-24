dbd326962217e90195807ba58581e86db6c567dd
Bekmurod <ahmadjonovbekmurod06@gmail.com>
2024-06-24 16:25:56 +0500

    Carousel Model updated

--- api/admin.py
+++ api/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Book, Comment, Author, Category
+from .models import Book, Comment, Author, Category, Carousel
 
 admin.site.register(Book)
 admin.site.register(Comment)
 admin.site.register(Author)
 admin.site.register(Category)
+admin.site.register(Carousel)
